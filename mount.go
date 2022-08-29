e3d2791d98658cca595856b70fe3e37e3169b993
Kousuke Kawahira <kawahira@binzume.net>
2022-08-30 01:49:13 +0900

    Copy example and interfacs from dkango

--- mount.go
+++ mount.go
@@ -1,5 +1,40 @@
 package fsmount
 
+import (
+	"io"
+	"io/fs"
+)
+
 type MountOptions struct {
 	FuseOption interface{}
+}
+
+type OpenWriterFS interface {
+	fs.FS
+	OpenWriter(name string, flag int) (io.WriteCloser, error)
+}
+
+type RemoveFS interface {
+	fs.FS
+	Remove(name string) error
+}
+
+type RenameFS interface {
+	fs.FS
+	Rename(name string, newName string) error
+}
+
+type MkdirFS interface {
+	fs.FS
+	Mkdir(name string, mode fs.FileMode) error
+}
+
+type OpenDirFS interface {
+	fs.FS
+	OpenDir(name string) (fs.ReadDirFile, error)
+}
+
+type TruncateFS interface {
+	fs.FS
+	Truncate(name string, size int64) error
 }
